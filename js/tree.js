a5ffc5185dfbd4d3695d98c1c3ca3f6336b13e04
Raphael Amorim <rapha850@gmail.com>
2014-02-27 00:10:08 -0300

    start :)

---
+++ js/tree.js
@@ -0,0 +1,10 @@
+/* Tree */
+window.onload = function() {
+
+    var create = document.querySelector('.btn-create');
+
+    create.addEventListener("click", function() {
+        console.log("blade");
+    });
+
+};
