d075543eb1b0ed804b66bb4a028ba9e0a0eeca57
Raphael Amorim <rapha850@gmail.com>
2014-03-02 23:03:33 -0300

    create tree working :D

--- js/tree.js
+++ js/tree.js
@@ -1,86 +1,45 @@
-/* Start */
-window.onload = function() {
-    var create = document.querySelector('.btn-create');
+/*
+  var treeDataJs = {
+      name    : "TreeData.js",
+      author  : "Raphael Amorim",
+      email   : "rapha850@gmail.com",
+      github  : "https://github.com/raphamorim/treeStructure"
+  };
+*/
 
-    create.addEventListener('click', function() {
-        var banana = prompt('Tell us the elements, separate with space or comma');
-        var inputs = banana.split(/[ ,]+/);
-
-        var sort = inputs.sort(function (a,b){
-          return a-b
-        });
-
-        var elements = sort.filter(function (value) {
-          return (typeof value === 'string');
-        });
-
-        console.log(elements);
-
-        if(elements.length > 1)
-          Tree(elements)
-        else
-          alert('Please sent than more of two valid itens')
-    }, false);
-
-};
-
-
-/* Tree */
-function Tree(el) {
-    cleanTree();
-
-    var container = document.querySelector('#container');
-    var main = document.querySelector('#main');
+/* Receive data and create tree */
+function Tree (treeData, select) {
+    var main = document.querySelector(select);
 
     var treecanvas = document.createElement('div');
         treecanvas.className = 'tree';
 
-    var htmlCode = '<ul>';
+    var treeCode = buildTree(treeData, "father");
 
-    for (var i in el) {
-        var actual = el[i];
-        var next = el[1 + parseInt(i)];
-        var futureNext = el[2 + parseInt(i)];
-
-        if(next > futureNext)
-           htmlCode += node(actual, futureNext, next)
-        else
-           htmlCode += node(actual, next, futureNext)
-
-        htmlCode += '</ul>';
-
-        el.splice(0, 2);
-	  }
-
-    treecanvas.innerHTML = htmlCode;
-
-    console.log(treecanvas);
+    treecanvas.innerHTML = treeCode;
 
     main.appendChild(treecanvas);
-
-    fadeOut(container);
-    fadeIn(main);
 }
 
-/* Temporaly Function */
-function cleanTree(){
-    var main = document.querySelector('#main');
-    main.innerHTML = '';
-}
+/* Recursive function to build tree structure :) */
+function buildTree (obj, node) {
+    treeString = "<li><a href='#'>" + obj[node].value + "</a>";
+    var sons = [];
 
+    for (var i in obj) {
+        if (obj[i].parent == node)
+          sons += i;
+    }
 
-/* Templates */
-function node(father, firstSon, secondSon) {
-  var node = [];
+    if (sons.length > 0) {
+        treeString += "<ul>";
 
-    if(father != undefined)
-        node += '<li><a href="#">' + father + '</a>';
+        for (var i in sons) {
+          treeString += buildTree(obj, sons[i]);
+        }
 
-    if(firstSon != undefined)
-        node += '<ul><li><a href="#">' + firstSon + '</a></li>';
+        treeString += "</ul>";
+    }
 
-    if(secondSon != undefined)
-        node += '<li><a href="#">' + secondSon + '</a></li>';
-
-    return node;
+    return treeString;
 }
